45714ef6f5f1821f1aba64491ab35138c9503483
Quypham <145001011+ImNotTurtle@users.noreply.github.com>
2024-09-30 12:44:47 +0700

    Add files via upload

--- VideoDoorBell_MonitorUnit/VideoDoorBell_MonitorUnit/src/main.cpp
+++ VideoDoorBell_MonitorUnit/VideoDoorBell_MonitorUnit/src/main.cpp
@@ -1,59 +1,23 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
-#include <driver/i2s.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <TFT_eSPI.h>
-// #include <SPI.h>
+#include <SPI.h>
 #include <TJpg_Decoder.h>
 #include <ESPAsyncWebServer.h>
 #include <WebSocketsClient.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 
-// Cấu hình I2S
-#define I2S_SAMPLE_RATE   (16000)        // Tần số mẫu (16kHz)
-#define I2S_PIN_BCK       (26)           // Chân BCK
-#define I2S_PIN_WS        (25)           // Chân WS
-#define I2S_PIN_DOUT      (22)           // Chân DOUT
-
 const char *ssid = "ESP32-CAM-AP";
 const char *password = "12345678";
-IPAddress APIP(192, 168, 4, 1); // IP of Access Point (ESP32 - CAM)
 uint8_t webSocketPort = 81;
 TFT_eSPI tft = TFT_eSPI();
 WebSocketsClient webSocket;
 bool m_wifiConnected = false;
 uint8_t imageBuffer[4096 * 3] = {0};
 size_t imageLength = 0;
-
-void setupI2S() {
-    i2s_config_t i2s_config = {
-        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),  // Chế độ Master và Transmit (TX)
-        .sample_rate = I2S_SAMPLE_RATE,
-        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,         // Mẫu 16-bit
-        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,         // Âm thanh 2 kênh (stereo)
-        .communication_format = I2S_COMM_FORMAT_I2S,          // Chuẩn I2S
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,             // Ngắt cấp 1
-        .dma_buf_count = 8,                                   // Bộ đệm DMA
-        .dma_buf_len = 64,                                    // Độ dài bộ đệm
-        .use_apll = false,
-        .tx_desc_auto_clear = true,
-        .fixed_mclk = 0
-    };
-
-    i2s_pin_config_t pin_config = {
-        .bck_io_num = I2S_PIN_BCK,
-        .ws_io_num = I2S_PIN_WS,
-        .data_out_num = I2S_PIN_DOUT,
-        .data_in_num = -1   // Không nhận input
-    };
-
-    // Thiết lập I2S driver
-    i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
-    i2s_set_pin(I2S_NUM_0, &pin_config);
-    i2s_set_clk(I2S_NUM_0, I2S_SAMPLE_RATE, I2S_BITS_PER_SAMPLE_16BIT, I2S_CHANNEL_STEREO);
-}
 
 bool tftOutput(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap);
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length);
@@ -89,8 +53,8 @@ void setup()
   tft.setTextFont(4);
   TJpgDec.setJpgScale(1);
   TJpgDec.setCallback(tftOutput);
-
-  webSocket.begin(APIP.toString(), webSocketPort, "/"); // 192.168.4.1:81/
+  
+  webSocket.begin(WiFi.gatewayIP().toString(), webSocketPort, "/"); // 192.168.4.1:81/
   webSocket.onEvent(webSocketEvent);
   webSocket.setReconnectInterval(5000);
 
